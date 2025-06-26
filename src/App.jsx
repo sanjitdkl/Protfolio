2f350eb27eb88f5aa66ab239d7ee2666d521d0ef
Sanjit Dhakal <dhakalsanjit6@gmail.com>
2025-06-26 19:28:59 +0545

    Add Project component with initial structure and placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Component/NavBar/NavBar";
 import Hero from "./Component/Hero/Hero";
 import About from "./Component/AboutSection/About";
+import Project from "./Component/Projectes/Project";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Project />
       <Routes>
         <Route path="/" element={<Hero />} />
-        <Route path="/about" element={<About />} /> {/* 👈 Add this line */}
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
---
+++ src/Component/Projectes/Project.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import KhajaKhana from "../../../public/KhajaKhana.png";
+const Project = () => {
+  return (
+    <div>
+      {/* <div>
+        <a href="https://khajakhana.netlify.app/">
+          <img src={KhajaKhana.png} />
+        </a>
+      </div> */}
+      <div className="bg-black text"> HEllO FROM PROJRECT</div>
+    </div>
+  );
+};
+
+export default Project;
