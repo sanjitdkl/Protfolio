70b4a1f7138f5dc8bc49f9351d1a96e24b17e012
Sanjit Dhakal <dhakalsanjit6@gmail.com>
2025-06-27 22:14:53 +0545

    Remove commented-out code for KhajaKhana link in Project component

--- src/Component/Projectes/Project.jsx
+++ src/Component/Projectes/Project.jsx
@@ -3,11 +3,6 @@ import KhajaKhana from "../../../public/KhajaKhana.png";
 const Project = () => {
   return (
     <div>
-      {/* <div>
-        <a href="https://khajakhana.netlify.app/">
-          <img src={KhajaKhana.png} />
-        </a>
-      </div> */}
       <div className="bg-black text"> HEllO FROM PROJRECT</div>
     </div>
   );
